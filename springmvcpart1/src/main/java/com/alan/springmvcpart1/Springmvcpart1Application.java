383e3e06eea2988c47f2c0f10aca5422efd11b4a
Alan <55138532+seongkyu-lim@users.noreply.github.com>
2022-12-31 10:23:56 +0000

    Init Project

---
+++ springmvcpart1/src/main/java/com/alan/springmvcpart1/Springmvcpart1Application.java
@@ -0,0 +1,13 @@
+package com.alan.springmvcpart1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springmvcpart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springmvcpart1Application.class, args);
+	}
+
+}
---
+++ springmvcpart1/src/test/java/com/alan/springmvcpart1/Springmvcpart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alan.springmvcpart1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springmvcpart1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
