e9ce230658623946d45e5cf59df31389a84b34e5
Alan <55138532+seongkyu-lim@users.noreply.github.com>
2023-01-06 04:12:12 +0000

    Feature: HelloServlet
    특정 경로에 대하여 HttpServlet의 request, response 커스터마이징 해보기

--- springmvcpart1/src/main/java/com/alan/springmvcpart1/Springmvcpart1Application.java
+++ springmvcpart1/src/main/java/com/alan/springmvcpart1/Springmvcpart1Application.java
@@ -2,7 +2,9 @@ package com.alan.springmvcpart1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class Springmvcpart1Application {
 
